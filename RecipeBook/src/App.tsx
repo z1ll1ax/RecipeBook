a38fe14be3e17aa8615d9c521d7c76e6ca79c351
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-12-24 14:00:06 +0300

    init: first commit

---
+++ RecipeBook/src/App.tsx
@@ -0,0 +1,36 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header className='header'>
+        <a className='logo'>
+
+        </a>
+        <input>
+
+        </input>
+      </header>
+      <main className='main'>
+        <aside className='menu'>
+          <section className='menu__section'>
+            Главная
+          </section>
+          <section className='menu__section'>
+            Мои рецепты
+          </section>
+        </aside>
+        <div className='catalog'>
+          <article className='catalog-item'>
+
+          </article>
+        </div>
+      </main>
+      <footer className='footer'>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
