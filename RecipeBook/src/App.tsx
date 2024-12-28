64213755acece4161390aabe02a8e957cc0a9aa7
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-12-28 20:47:28 +0300

    feat: header block

--- RecipeBook/src/App.tsx
+++ RecipeBook/src/App.tsx
@@ -1,15 +1,30 @@
 import './App.scss';
+import logoImage from '../src/assets/images/logo.png';
+import accountEmotyImage from '../src/assets/images/account_empty_image.svg';
 
 function App() {
   return (
     <>
       <header className='header'>
-        <a className='logo'>
-
-        </a>
-        <input>
-
-        </input>
+        <div className='header-overflow'>
+          <a className='header-logo'>
+            <img className='header-logo__image'
+                  src={logoImage}
+                  alt='Logo'></img>
+            <h1 className='header-logo__header'>RecipeBook</h1>
+          </a>
+          <input className='header-input'
+                placeholder='Поиск...'
+                type='text'>
+          </input>
+          <a className='account'>
+            <img className='account__avatar' src={accountEmotyImage}></img>
+            <div className='account-settings'>
+              <h3 className='account-settings__name'>Иван Иванов</h3>
+              <a className='account-settings__quit'>Выйти</a>
+            </div>
+          </a>
+        </div>
       </header>
       <main className='main'>
         <aside className='menu'>
